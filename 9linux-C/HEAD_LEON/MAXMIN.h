293e17f4085f3b4e4318142f2293dd2d6c4ba11d
Leon1023 <liangzc1124@163.com>
2019-02-15 15:43:20 +0800

    add macro of MAX(x,y) and MIN(x,y)

---
+++ 9linux-C/HEAD_LEON/MAXMIN.h
@@ -0,0 +1,13 @@
+#define MIN(x,y) ({\
+        typeof(x) _x = x;\
+        typeof(y) _y = y;\
+        (void)( &_x == &_y );\
+        _x < _y ? _x : _y;\
+        })
+
+#define MAX(x,y) ({\
+        typeof(x) _x = x;\
+        typeof(y) _y = y;\
+        (void)( &_x == &_y );\
+        _x > _y ? _x : _y;\
+        })
